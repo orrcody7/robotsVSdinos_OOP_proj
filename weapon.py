8f8ab26425784891e3e7f5367bc89d2a3034723a
unknown <orrcody7@gmail.com>
2021-08-10 13:01:45 -0600

    created a robot.py class and imported the weapon.py file in to the robot class to add the weapon class to the robots

---
+++ weapon.py
@@ -0,0 +1,15 @@
+## TODO: Make a class for Weapons
+
+class Weapon:
+    # ~~~~~ CONSTRUCTOR ~~~~~
+    def __init__(self, name, attack_power):
+        self.weap_type = "laser gun"
+        self.attack_pwr = 25
+
+    # ~~~~~ METHODS ~~~~~
+
+    def weapon_specs(self, name):
+        pass
+
+    def weap_atk_pwr(self):
+        pass
